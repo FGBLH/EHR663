c8de46c227099a9ba9693fe847367333b6e252b5
FGBLH <pengli08211@gmail.com>
2026-09-12 12:28:02 +0800

    Update 智能加载v5.1.py

--- py/智能加载v5.1.py
+++ py/智能加载v5.1.py
@@ -14,10 +14,10 @@ class Spider(Spider):
     # ==========================================================================
     # 📂 【配置区】
     # ==========================================================================
-    PY_DIR    = "/storage/emulated/0/海豚影视/海豚/py"
-    JS_DIR    = "/storage/emulated/0/海豚影视/海豚/js"
-    JAR_DIR   = "/storage/emulated/0/海豚影视/海豚/jar"
-    SAVE_PATH = "/storage/emulated/0/海豚影视/海豚/海豚影视.json"
+    PY_DIR    = "/storage/emulated/0/peekpro/py"
+    JS_DIR    = "/storage/emulated/0/peekpro/js"
+    JAR_DIR   = "/storage/emulated/0/peekpro/jar"
+    SAVE_PATH = "/storage/emulated/0/peekpro/智能接口.json"
     LOGO_PATH = "/https://img.freepik.com/free-vector/cute-dolphin-swimming-cartoon-vector-icon-illustration-animal-nature-icon-isolated-flat-vector_138676-12582.jpg?semt=ais_hybrid&w=740&q=80"
 
     # 🔒 锁定在 sites 第 0、1 位的配置，无论扫描结果如何始终存在
